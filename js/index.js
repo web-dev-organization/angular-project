347621c5fc07422f3350cee3141c0b00788f62a9
tonight111 <xiashang246@qq.com>
2019-10-20 22:05:04 +0800

    1

--- js/index.js
+++ js/index.js
@@ -6,6 +6,41 @@ myapp.controller("indexcontroller",function($scope){
           alert("AAA");
     }
 
-    
+     var object1 = {
+                No:"1",
+                memberId:"00001",
+                memberName:"user1",
+                mailAddress:"sample1@gmail.com",
+                phoneNum:"111-1111-1111",
+                place:"000省000市000区000路",
+                memberCreatedAt:"2020/01/01",
+                memberStatus:"普通"
+            }
+   
+
+    var object2 = {
+                No:"2",
+                memberId:"00002",
+                memberName:"user2",
+                mailAddress:"sample1@gmail.com",
+                phoneNum:"111-1111-1111",
+                place:"000省000市000区000路",
+                memberCreatedAt:"2020/01/01",
+                memberStatus:"普通"
+            }
+        
+
+    var object3 = {
+                No:"3",
+                memberId:"00003",
+                memberName:"user3",
+                mailAddress:"sample1@gmail.com",
+                phoneNum:"111-1111-1111",
+                place:"000省000市000区000路",
+                memberCreatedAt:"2020/01/01",
+                memberStatus:"普通"
+            }
+
+    $scope.mylist=[object1,object2,object3];
 
 });
